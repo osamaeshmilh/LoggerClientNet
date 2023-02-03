a5d3b56d90641401b593b138dce25a46c6e932c9
osamaeshmilh <osamaeshmilh@gmail.com>
2023-02-03 12:44:13 +0000

    add Kafka

--- LoggerClient/AppLogger.cs
+++ LoggerClient/AppLogger.cs
@@ -1,9 +1,21 @@
-﻿namespace LoggerClient
+﻿using Confluent.Kafka;
+
+namespace LoggerClient
 {
     public class AppLogger
     {
-        public void Log(string text)
+        public async Task LogAsync(string text)
         {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "host1:9092",
+                
+            };
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                var result = await producer.ProduceAsync("weblog", new Message<Null, string> { Value = "a log message" });
+            }
             Console.WriteLine(text);
         }
     }
